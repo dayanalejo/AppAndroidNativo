d6ab5359e5e82e7bccef0d403212d15c9b39676c
Dayana Alejo <dayanalejo@gmail.com>
2016-11-17 09:50:43 -0500

    mapa de bogota

--- app/src/main/java/com/cappcitate/dayanavaio/proyectotransmilenio/MapaDeBogota.java
+++ app/src/main/java/com/cappcitate/dayanavaio/proyectotransmilenio/MapaDeBogota.java
@@ -3,7 +3,7 @@ package com.cappcitate.dayanavaio.proyectotransmilenio;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
-import com.google.android.gms.maps.GoogleMap;
+//import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
