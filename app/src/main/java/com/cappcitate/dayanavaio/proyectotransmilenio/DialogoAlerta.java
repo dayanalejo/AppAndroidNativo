8154a6408cb2172a5a7865845c571a34f7d2640e
Dayana Alejo <dayanalejo@gmail.com>
2016-11-17 09:50:55 -0500

    dialogo cambie el mensaje

--- app/src/main/java/com/cappcitate/dayanavaio/proyectotransmilenio/DialogoAlerta.java
+++ app/src/main/java/com/cappcitate/dayanavaio/proyectotransmilenio/DialogoAlerta.java
@@ -3,7 +3,7 @@ package com.cappcitate.dayanavaio.proyectotransmilenio;
 import android.app.AlertDialog;
 //import android.app.Dialog;
 import android.app.DialogFragment;
-import android.os.Bundle;
+//import android.os.Bundle;
 
 
 public class DialogoAlerta import DialogFragment {
