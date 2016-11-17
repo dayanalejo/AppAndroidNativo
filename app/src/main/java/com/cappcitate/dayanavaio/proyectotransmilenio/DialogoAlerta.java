15753f575fe075762ec9c3768d112d376226dd31
Dayana Alejo <dayanalejo@gmail.com>
2016-11-17 09:42:37 -0500

    esta libreria ya no se esta usando
    ss

--- app/src/main/java/com/cappcitate/dayanavaio/proyectotransmilenio/DialogoAlerta.java
+++ app/src/main/java/com/cappcitate/dayanavaio/proyectotransmilenio/DialogoAlerta.java
@@ -1,7 +1,7 @@
 package com.cappcitate.dayanavaio.proyectotransmilenio;
 
 import android.app.AlertDialog;
-import android.app.Dialog;
+//import android.app.Dialog;
 import android.app.DialogFragment;
 import android.os.Bundle;
 
